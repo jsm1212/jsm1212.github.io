e03ab88009f177c5b621ff578e7fc5cb6016a498
jsm <jun0107728@naver.com>
2021-07-10 23:13:18 +0900

    20210710COMMIT

---
+++ works/js/weather.js
@@ -0,0 +1,21 @@
+const weather = document.querySelector("#weather span:first-child")
+const city = document.querySelector("#weather span:last-child")
+const API_KEY = "062cb52590492d0302826568a5732707"
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
